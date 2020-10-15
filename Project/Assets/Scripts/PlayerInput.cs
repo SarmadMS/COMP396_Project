3d3ea99412cee8355b9fcfa70c09ad249901aa64
Lyle <lylespurrell@hotmail.com>
2020-10-14 22:25:19 -0400

    Drop in / drop out player 2 split screen
    - plug in 2 Xbox 360 controllers
    - press the start button on the 2nd controller to split screen
    - press start again to switch back to player 1 fullscreen

---
+++ Project/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private GameObject player2;
+    [SerializeField] private Camera player1Camera;
+
+    private bool split = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeScreen();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("P2Start"))
+        {
+            ChangeScreen();
+        }
+    }
+
+    void ChangeScreen()
+    {
+        if (split == false)
+        {
+            player2.SetActive(false);
+            player1Camera.rect = new Rect(0, 0, 1, 1);
+            split = true;
+        }
+        else
+        {
+            player2.SetActive(true);
+            player1Camera.rect = new Rect(0, 0.5f, 1, 0.5f);
+            split = false;
+        }
+    }
+}
