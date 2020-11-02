e2408b70b8c8d0ad39adefdb565dd3fd77d8f432
Reneton20 <reneton13@hotmail.com>
2020-11-01 23:01:38 -0500

    Early-Build Not Final, Just a Sample
    Instructions to test this one out:
    1. Go to folder with my name
    2. Go to Scenes
    3. Click on MenuEarly
    4. Click Play
    5. Click "Play Bootleg Game" and options don't work (yet)

---
+++ Project/Assets/Reneton/Scripts/TestScene.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestScene : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
