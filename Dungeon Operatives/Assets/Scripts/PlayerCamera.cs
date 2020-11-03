42cf10efaf7a37a4072165421ce56eae2df753eb
Timothy Garcia <timothyiangarcia@gmail.com>
2020-11-03 18:06:28 -0500

    Camera and Movement
    Fixed camera Movement for Player

---
+++ Dungeon Operatives/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("P1 Look X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("P1 Look Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation += mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
