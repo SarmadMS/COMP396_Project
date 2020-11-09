90a35d54327a0d45052fa70ec636416d6202b97a
Timothy Garcia <timothyiangarcia@gmail.com>
2020-11-08 20:06:34 -0500

    Enemy AI
    Enemy AI Scene added and Chasing Enemy script added.

---
+++ Dungeon Operatives/Assets/Scripts/ChaseEnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChaseEnemyController : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+    }
+}
