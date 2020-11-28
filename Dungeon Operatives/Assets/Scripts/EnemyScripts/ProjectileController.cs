2e437364dcd59eb8441be1556a530a42cddbbcda
Timothy Garcia <timothyiangarcia@gmail.com>
2020-11-28 03:21:02 -0500

    Updated enemy projectile to be destroyed on collision with player/walls.

---
+++ Dungeon Operatives/Assets/Scripts/EnemyScripts/ProjectileController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    public Rigidbody rb;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == player)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
