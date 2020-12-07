070d0d9ebbfad5e430bb63e6b475898fd551b903
Timothy Garcia <timothyiangarcia@gmail.com>
2020-12-07 15:27:09 -0500

    Start button functionality (Level1 Scene)

---
+++ Dungeon Operatives/Assets/Scripts/SplitScreenController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplitScreenController : MonoBehaviour
+{
+    public Camera playerCam1, playerCam2;
+    public bool isHorizontal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonUp("P1 Start"))
+        {
+            isHorizontal = !isHorizontal;
+            SplitScreen();
+        }
+    }
+
+    public void SplitScreen()
+    {
+        if (isHorizontal)
+        {
+            playerCam1.rect = new Rect(0f, 0.5f, 1f, 0.5f);
+            playerCam2.rect = new Rect(0f, 0f, 1f, 0.5f);
+        }
+        else
+        {
+            playerCam1.rect = new Rect(0f, 0f, 0.5f, 1f);
+            playerCam2.rect = new Rect(0.5f, 0f, 0.5f, 1f);
+        }
+    }
+}
