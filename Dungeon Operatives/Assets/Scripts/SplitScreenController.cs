a64ea245b65473e404c78a868c8e6d279d638e3b
Lyle <lylespurrell@hotmail.com>
2020-12-09 18:36:44 -0500

    Improved SplitScreenController  Script
    p1 start - solo / split
    p2 start - split v / split h

--- Dungeon Operatives/Assets/Scripts/SplitScreenController.cs
+++ Dungeon Operatives/Assets/Scripts/SplitScreenController.cs
@@ -5,34 +5,61 @@ using UnityEngine;
 public class SplitScreenController : MonoBehaviour
 {
     public Camera playerCam1, playerCam2;
-    public bool isHorizontal;
+    public bool isSolo = true;
+    public bool isHorizontal = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonUp("P1 Start"))
+        if (Input.GetButtonUp("P1 Start"))
         {
-            isHorizontal = !isHorizontal;
+            Debug.Log("p1 start");
             SplitScreen();
+        }
+
+        if (Input.GetButtonUp("P2 Start"))
+        {
+            Debug.Log("p2 start");
+            ChangeSplit();
         }
     }
 
     public void SplitScreen()
     {
-        if (isHorizontal)
+        if (isSolo == true)
         {
+            isSolo = false;
             playerCam1.rect = new Rect(0f, 0.5f, 1f, 0.5f);
             playerCam2.rect = new Rect(0f, 0f, 1f, 0.5f);
+            isHorizontal = true;
         }
         else
         {
+            isSolo = true;
+            playerCam1.rect = new Rect(0f, 0f, 1f, 1f);
+            playerCam2.rect = new Rect(0f, 0f, 0f, 0f);
+        }
+    }
+
+    public void ChangeSplit()
+    {
+        if (isHorizontal == true)
+        {
             playerCam1.rect = new Rect(0f, 0f, 0.5f, 1f);
             playerCam2.rect = new Rect(0.5f, 0f, 0.5f, 1f);
+            isHorizontal = false;
+        }
+        else
+        {
+            playerCam1.rect = new Rect(0f, 0.5f, 1f, 0.5f);
+            playerCam2.rect = new Rect(0f, 0f, 1f, 0.5f);
+            isHorizontal = true;
         }
     }
 }
